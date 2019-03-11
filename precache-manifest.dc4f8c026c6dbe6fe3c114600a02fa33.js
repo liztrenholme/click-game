ebc81e3a5dc666347a5916202fdb7d18db17bd79
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-03-10 21:30:12 -0400

    Updates

---
+++ precache-manifest.dc4f8c026c6dbe6fe3c114600a02fa33.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b504a5610b136099eeb",
+    "url": "/click-game/static/css/main.8c570971.chunk.css"
+  },
+  {
+    "revision": "9b504a5610b136099eeb",
+    "url": "/click-game/static/js/main.cc0336aa.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  },
+  {
+    "revision": "13261f48d341b80ce102",
+    "url": "/click-game/static/js/2.b7e45837.chunk.js"
+  },
+  {
+    "revision": "069914b83da83d8fefccab734298ccdc",
+    "url": "/click-game/static/media/galaxy.069914b8.jpg"
+  },
+  {
+    "revision": "307691e39e3c96a96cbae140b220addd",
+    "url": "/click-game/static/media/hubble.307691e3.jpg"
+  },
+  {
+    "revision": "12265bd954a1fb0e5815ee77ce7fb614",
+    "url": "/click-game/static/media/pic1.12265bd9.jpg"
+  },
+  {
+    "revision": "2860bce3420c28b5f524eb81c7bcb303",
+    "url": "/click-game/static/media/pic2.2860bce3.jpg"
+  },
+  {
+    "revision": "0c3be32bedd0fea95d6785d06149e339",
+    "url": "/click-game/static/media/pic3.0c3be32b.jpg"
+  },
+  {
+    "revision": "d286c5d87f2c2286f031625d89dac26f",
+    "url": "/click-game/static/media/pic4.d286c5d8.jpg"
+  },
+  {
+    "revision": "f5477d7458acf0b8272e0b7c0845e6a0",
+    "url": "/click-game/static/media/pic5.f5477d74.jpg"
+  },
+  {
+    "revision": "97afc2ee35a84cdcf64137ee07c54153",
+    "url": "/click-game/static/media/pic6.97afc2ee.jpg"
+  },
+  {
+    "revision": "05494c7be66ceea36679b44d0c88697b",
+    "url": "/click-game/static/media/pic7.05494c7b.jpg"
+  },
+  {
+    "revision": "50827dc38d6e7844a247b281d1365f1b",
+    "url": "/click-game/static/media/pic8.50827dc3.jpg"
+  },
+  {
+    "revision": "bca63c138027bc61f55cb929a76be941",
+    "url": "/click-game/static/media/pic9.bca63c13.jpg"
+  },
+  {
+    "revision": "bf865e8eecab83c133e8da66a8784dfd",
+    "url": "/click-game/static/media/win.bf865e8e.jpg"
+  },
+  {
+    "revision": "91f6354d1efeff060f4690a40ab39f9d",
+    "url": "/click-game/static/media/gameover.91f6354d.jpg"
+  },
+  {
+    "revision": "d45046e41834c800c2392f2c2a4425e6",
+    "url": "/click-game/static/media/galaxy3.d45046e4.jpg"
+  },
+  {
+    "revision": "8066ee710802746761e2535f71a93254",
+    "url": "/click-game/index.html"
+  }
+];
